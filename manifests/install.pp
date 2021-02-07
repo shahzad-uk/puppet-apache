42108958e58469bdf5a5475764363e236a16280d
Shahzad Arif <sarif.hk@gmail.com>
2021-02-07 21:04:38 +0000

    Create install class for apache

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,8 @@
-# @summary A short summary of the purpose of this class
+# @summary 
+# Installs the base Apache package
 #
-# A description of what this class does
-#
-# @example
-#   include apache::install
 class apache::install {
+  package { 'httpd':
+    ensure => 'present',
+  }
 }
