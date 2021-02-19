7ffbed2c0144a0bb35a1d965cd63387b47ce4df5
Shahzad Arif <sarif.hk@gmail.com>
2021-02-19 22:17:06 +0000

    commenting out error code

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,20 +4,20 @@
 #
 # @example
 #   include apache
-class apache(
+class apache (
   String $install_name,
   String $install_ensure,
   String $config_ensure,
   String $config_path,
-  Enum["running", "stopped"] $service_ensure,
-  String $service_name,
-  Boolean $service_enable,
+#  Enum["running", "stopped"] $service_ensure,
+#  String $service_name,
+#  Boolean $service_enable,
 ) {
   contain apache::install
   contain apache::config
-  contain apache::service
+#  contain apache::service
 
   Class['::apache::install'] 
   -> Class['::apache::config']
-  ~> Class['::apache::service']
+#  ~> Class['::apache::service']
 }
